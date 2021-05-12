c048db6364f3c958f9e8ff5ed596028bbc86707f
Sina <sina.alsharif@gmail.com>
2021-05-12 23:03:40 +1000

    Error handling

--- scripts/scrape.py
+++ scripts/scrape.py
@@ -29,8 +29,12 @@ def get_results(url):
     input_box = browser.find_element_by_class_name("input")
     output_box = browser.find_element_by_class_name("output")
 
-    inp = input_box.find_elements_by_tag_name("pre")[0]
-    out = output_box.find_elements_by_tag_name("pre")[0]
+    try:
+        inp = input_box.find_elements_by_tag_name("pre")[0]
+        out = output_box.find_elements_by_tag_name("pre")[0]
+    except:
+        print("Elements not found")
+        return
 
     print(inp.text)
     print(out.text)
